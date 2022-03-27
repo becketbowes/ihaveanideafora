7833270d696b9e6297a059a1a32dd4541bee4aa3
becket <becket.bowes@gmail.com>
2022-03-27 19:47:55 -0400

    reformat index

--- Model/index.js
+++ Model/index.js
@@ -4,85 +4,18 @@ const Comment = require('./Comment');
 const Upvote = require('./Upvote');
 const Conversation = require('./Conversation');
 
-User.hasMany(Idea, {
-    foreignKey: 'userkey',
-    onDelete: 'SET NULL'
-});
-
-Idea.belongsTo(User, {
-    foreignKey: 'userkey',
-    onDelete: 'SET NULL'
-    // Do we need ONDELETE?  Should we keep ideas even if user is deleted?
-});
-
-User.belongsToMany(Idea, {
-    through: Upvote,
-    as: 'upvoted_ideas',
-    foreignKey: 'userkey',
-    onDelete: 'SET NULL'
-    // We should match previous ONDELETE decision
-});
-
-Idea.belongsToMany(User, {
-    through: Upvote,
-    as: 'upvoted_ideas',
-    foreignKey: 'ideakey',
-    onDelete: 'SET NULL'
-});
-
-Upvote.belongsTo(User, {
-    foreignKey: 'userkey',
-    onDelete: 'SET NULL'
-});
-
-Upvote.belongsTo(Idea, {
-    foreignKey: 'ideakey',
-    onDelete: 'SET NULL'
-});
-
-User.hasMany(Upvote, {
-    foreignKey: 'userkey',
-    onDelete: 'SET NULL'
-});
-
-Idea.hasMany(Upvote, {
-    foreignKey: 'ideakey',
-    onDelete: 'SET NULL'
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'userkey',
-    onDelete: 'SET NULL'
-    //Same question Re: ONDELETE
-});
-
-Comment.belongsTo(Idea, {
-    foreignKey: 'ideakey',
-    onDelete: 'SET NULL'
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'userkey',
-    onDelete: 'SET NULL'
-    //Same onDelete question
-});
-
-Idea.hasMany(Comment, {
-    foreignKey: 'ideakey',
-    onDelete: 'SET NULL'
-});
-
-User.belongsToMany(Conversation, {
-    through: 
-})
-
-User.hasMany(Conversation, {
-    foreignKey: 'userkey',
-    onDelete: 'SET NULL'
-});
-
-
-
-
+User.hasMany(Idea, { foreignKey: 'userkey', onDelete: 'SET NULL' });
+User.belongsToMany(Idea, { through: Upvote, as: 'upvoted_ideas', foreignKey: 'userkey', onDelete: 'SET NULL' });
+User.hasMany(Conversation, { foreignKey: 'userkey', onDelete: 'SET NULL' });
+User.hasMany(Comment, { foreignKey: 'userkey', onDelete: 'SET NULL' });
+User.hasMany(Upvote, { foreignKey: 'userkey', onDelete: 'SET NULL' });
+Idea.belongsTo(User, { foreignKey: 'userkey', onDelete: 'SET NULL' });
+Idea.belongsToMany(User, { through: Upvote, as: 'upvoted_ideas', foreignKey: 'ideakey', onDelete: 'SET NULL' });
+Idea.hasMany(Comment, { foreignKey: 'ideakey', onDelete: 'SET NULL' });
+Idea.hasMany(Upvote, { foreignKey: 'ideakey', onDelete: 'SET NULL' });
+Comment.belongsTo(User, { foreignKey: 'userkey', onDelete: 'SET NULL' });
+Comment.belongsTo(Idea, { foreignKey: 'ideakey', onDelete: 'SET NULL' });
+Upvote.belongsTo(Idea, { foreignKey: 'ideakey', onDelete: 'SET NULL' });
+Upvote.belongsTo(User, { foreignKey: 'userkey', onDelete: 'SET NULL' });
 
 module.exports = { User, Idea, Upvote, Comment, Conversation };
