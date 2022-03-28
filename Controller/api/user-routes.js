9f2e3ef8d7b7bb609dec566798c528abe7170500
MrSumada <ricksamericana.com>
2022-03-28 15:53:41 -0400

    add api/user/ login and logout routes

--- Controller/api/user-routes.js
+++ Controller/api/user-routes.js
@@ -75,6 +75,44 @@ router.post('/' , (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "no user found with this email" });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: "Incorrect password"});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.name = dbUserData.name;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: "You're logged in!"})
+        });
+    });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 // DELETE to Remove User  
 router.delete('/:id', (req, res) => {
     User.destroy({
--- Views/public/assets/js/login.js
+++ Views/public/assets/js/login.js
@@ -1,3 +1,42 @@
 //login call: onclick "loginbutton" verify user and res.render ideas page, else return error
 
 //signup call: onclick "signupbutton" create new user, res.render ideas page, else return error
+
+
+async function loginHandler(event) {
+    event.preventDefault();
+    
+    const email = document.querySelector('#login-email').value.trim();
+    const password = document.querySelector('#login-password').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email, password
+            }),
+            headers: { 'Content-Type': 'application/json'}
+        });
+
+        if (response.ok) {
+            document.location.assign('/')
+        } else {
+            alert(response.statusText)
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('.loginbutton').addEventListener("submit", loginHandler);
+document.querySelector('.signupbutton').addEventListener("submit", signupHandler);
