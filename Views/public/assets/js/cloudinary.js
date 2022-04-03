91731952db4b6dd426d80c7f85992020aaba8e33
becket <becket.bowes@gmail.com>
2022-04-03 00:46:26 -0400

    getting functional url for saved images returned

--- Views/public/assets/js/cloudinary.js
+++ Views/public/assets/js/cloudinary.js
@@ -1,37 +1,57 @@
 const cloudName = "ideafora"; // replace with your own cloud name
 const uploadPreset = "ideafora_format"; // replace with your own upload preset
-// const userNameId = document. // get user name and id from document
+const userNameId = document.querySelector('#userinfo'); // get user name and id from document
 
-// Remove the comments from the code below to add
-// additional functionality.
-// Note that these are only a few examples, to see
-// the full list of possible parameters that you
-// can add see:
-//   https://cloudinary.com/documentation/upload_widget_reference
 
 const imageDrop = cloudinary.createUploadWidget(
   {
     cloudName: cloudName,
     uploadPreset: uploadPreset,
-    // cropping: true, //add a cropping step
-    // showAdvancedOptions: true,  //add advanced options (public_id and tag)
-    // sources: [ "local", "url"], // restrict the upload sources to URL and local files
-    multiple: false,  //restrict upload to a single file
-    folder: "../img/userimg", //upload files to the specified folder
-    // tags: ["users", "profile"], //add the given tags to the uploaded files
-    context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
-    clientAllowedFormats: ["images"], //restrict uploading to image files only
+    cropping: true, 
+    showSkipCropButton: false,
+    croppingAspectRatio: 1,
+    multiple: false, 
+    folder: "userimg", //upload files to the specified folder
+    tags: [userNameId.user], //add the given tags to the uploaded files
+    // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
+    // clientAllowedFormats: ["images"], //restrict uploading to image files only
     maxImageFileSize: 2000000,  //restrict file size to less than 2MB
     maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
     theme: "minimal", //change to a minimal theme
-    // styles: createStyle //https://cloudinary.com/documentation/upload_widget#look_and_feel_customization
+    styles: {
+      palette: {
+        window: "#EBEDFF",
+        windowBorder: "#EBEDFF",
+        tabIcon: "#00053E",
+        menuIcons: "#EBEDFF",
+        textDark: "#00053E",
+        textLight: "#EBEDFF",
+        link:  "#00053E",
+        action:  "#00053E",
+        inactiveTabIcon: "#00053E",
+        error: "#00053E",
+        inProgress: "#EBEDFF",
+        complete: "#EBEDFF",
+        sourceBg: "#00053E"
+      },
+      frame: {
+        background: "#EBEDFF"
+      },
+      fonts: {
+          "'Inconsolata', 600": "https://fonts.googleapis.com/css2?family=Inconsolata:wght@600&display=swap",
+      }
+  }
   },
   (err, res) => {
     if (!err && res && res.event === "success") {
-      console.log("Done! Here is the image info: ", result.info);
+      console.log("Done! Here is the image info: ", res.info);
       document
         .getElementById("uploadedimage")
-        .setAttribute("src", result.info.secure_url);
+        .setAttribute("src", res.info.secure_url);
+      document.location.reload();
+        // // THIS !!!
+        // //how do i make this a secure url? also, make this function?
+        // .setAttribute("src", res.info.secure_url/userimg/(userNameId.user));;
     }
   }
 );
@@ -43,4 +63,3 @@ document.getElementById("addimage").addEventListener(
   },
   false
 );
-document.getElementById("addimage").addEventListener("click", function() { imageDrop.open(); }, false);
--- Views/public/assets/js/uploader.js
+++ Views/public/assets/js/uploader.js
@@ -2,13 +2,6 @@ const cloudName = "ideafora"; // replace with your own cloud name
 const uploadPreset = "ideafora_format"; // replace with your own upload preset
 const userNameId = document.querySelector('#userinfo'); // get user name and id from document
 
-// Remove the comments from the code below to add
-// additional functionality.
-// Note that these are only a few examples, to see
-// the full list of possible parameters that you
-// can add see:
-//   https://cloudinary.com/documentation/upload_widget_reference
-
 const imageDrop = cloudinary.createUploadWidget(
   {
     cloudName: cloudName,
@@ -24,7 +17,29 @@ const imageDrop = cloudinary.createUploadWidget(
     maxImageFileSize: 2000000,  //restrict file size to less than 2MB
     maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
     theme: "minimal", //change to a minimal theme
-    // styles: createStyle //https://cloudinary.com/documentation/upload_widget#look_and_feel_customization
+    styles: {
+        palette: {
+          window: "#000326",
+          windowBorder: "#000326",
+          tabIcon: "#EBEDFF",
+          menuIcons: "#EBEDFF",
+          textDark: "#EBEDFF",
+          textLight: "#EBEDFF",
+          link:  "#EBEDFF",
+          action:  "#EBEDFF",
+          inactiveTabIcon: "#C4C9FF",
+          error: "#EBEDFF",
+          inProgress: "#EBEDFF",
+          complete: "#00053E",
+          sourceBg: "#EBEDFF"
+        },
+        frame: {
+          background: "#000326"
+        },
+        fonts: {
+            "'Inconsolata', ": "https://fonts.googleapis.com/css2?family=Inconsolata:wght@600&display=swap",
+        }
+    }
   },
   (error, result) => {
     if (!error && result && result.event === "success") {
@@ -33,7 +48,7 @@ const imageDrop = cloudinary.createUploadWidget(
         .getElementById("uploadedimage")
 
         // THIS !!!
-        //how do i make this a secure url?
+        //how do i make this a secure url? also, function?
         .setAttribute("src", result.info.secure_url/userimg/(userNameId.user));
     }
   }
